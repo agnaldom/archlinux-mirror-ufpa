7e707c8c82e4a668c43666d2159148d948b3ccc0
Agnaldo Marinho <agnaldomarinho7@gmail.com>
2017-05-16 14:48:03 -0300

    Modifiquei como ele vai jogar no crontab

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #Generate a random time for syncing
 #echo "$(($RANDOM%60)) */2 * * * /mirror_archlinux.sh" > /cron
-echo "0 */1 * * * root /syncrepo.sh -s >/dev/null 2>&1" >> /etc/crontab
+echo "0 6 * * * root true *** Info: Mirroring *** | /usr/local/bin/syncrepo.sh -s >/dev/null 2>&1" >> /etc/crontab
 
 #add it
 #crontab /cron
